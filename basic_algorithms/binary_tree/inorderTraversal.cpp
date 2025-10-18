e46efec3efbe28b76bcf82570e7ec3124d54e123
alejandronaranjocaraza <216925605+alejandronaranjocaraza@users.noreply.github.com>
2025-10-18 14:24:26 -0600

    binary tree

---
+++ basic_algorithms/binary_tree/inorderTraversal.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+template <typename Object>
+struct Node {
+  Node(const Object& data = Object{}, Node<Object>* left = nullptr, Node<Object>* right = nullptr) :
+    data(data),left(left),right(right) {}
+  Node(Object&& data, Node<Object>* left = nullptr, Node<Object>* right = nullptr) :
+    data(std::move(data)), left(left), right(right) {}
+  void addLeft(const Object& val = Object{}) {
+    left = new Node<Object>(val);
+  }
+  void addRight(const Object& val = Object{}) {
+    right = new Node<Object>(val);
+  }
+  Object data;
+  Node<Object>* left;
+  Node<Object>* right;
+};
+
+void inorderPrint(Node<int>* root) {
+  if(root == nullptr) return;
+  
+  inorderPrint(root->left);
+  std::cout << root->data << std::endl;
+  inorderPrint(root->right);
+}
+//
+//          0
+//        /   \
+//       1     2
+//     /  \   /  \
+//    3    4 5    6
+//
+int main() {
+  Node<int>* root = new Node<int>(0);
+  root->addLeft(1);
+  root->addRight(2);
+  Node<int>* left1 = root->left;
+  Node<int>* right1 = root->right;
+  left1->addLeft(3);
+  left1->addRight(4);
+  right1->addLeft(5);
+  right1->addRight(6);
+  inorderPrint(root);
+  return 0;
+}
